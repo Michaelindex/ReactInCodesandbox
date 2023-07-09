5f9a03254fa1a0fa5b2a502bf0000c55a6efebcc
Michael Santos <113257529+Michaelindex@users.noreply.github.com>
2023-07-09 00:38:14 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styles.css";
 
+import Navbar from "./components/Navbar/Navbar";
+
 export default function App() {
   return (
     <div className="App">
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <span>React Kanban</span>
+    </nav>
+  );
+}
