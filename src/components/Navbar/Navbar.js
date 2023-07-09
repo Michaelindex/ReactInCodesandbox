9cd71960bf665995fef26a0104477faa53efeb99
Michael Santos <113257529+Michaelindex@users.noreply.github.com>
2023-07-09 00:41:56 +0000

    Update 2 files

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import react from "react";
+import "./navbar.css";
 
 export default function Navbar() {
   return (
-    <nav>
-      <span>React Kanban</span>
+    <nav className="navbar">
+      <span>react Kanban</span>
     </nav>
   );
 }
