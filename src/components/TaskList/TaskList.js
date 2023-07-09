31ca1c621d33dc9294625a3c39932fd767e29c73
Michael Santos <mcialbr123@gmail.com>
2023-07-08 22:06:25 -0300

    css

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function TaskList(props) {
     return(
         <div>
-            <div>Titulo</div>
+            <div>{props.title}</div>
         </div>
     )
 }
