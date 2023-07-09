3c6ccbe5c53be69ebebfa9f15e007ae9e35b370f
Michael Santos <mcialbr123@gmail.com>
2023-07-08 22:43:38 -0300

    update

--- src/App.js
+++ src/App.js
@@ -4,14 +4,38 @@ import "./styles.css";
 import Navbar from "./components/Navbar/Navbar"
 import TaskList from "./components/TaskList/TaskList";
 
+const task = {
+  id: 0,
+  title: 'Nova Tarefa',
+  state: 'Pendente'
+};
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+}
+
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    console.log("Funcao sendo chamada em App")
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return[...existingTasks, newTask]
+    })
+  }
+
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-        <TaskList title="Pendente" />
-        <TaskList title="Fazendo" />
-        <TaskList title="Completa" />
+        <TaskList title="Pendente" onAddTask={addTask} tasks={tasks} />
       </div>
     </div>
   );
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
 import "./tasklist.css"
 import PropTypes from "prop-types";
 
-export default function TaskList({ title }) {    
+export default function TaskList({ title, onAddTaskl, tasks }) {
+    console.log("Chamada dentro da Tasklist")
+
+    const addTask = () => {
+        onAddTask('Nova Tarefa', 'Pendente')
+    }
+
     return(
         <div className="tasklist">
             <div className="title">{title}</div>
             <div className="content">
+                {tasks.map((task) => {
+                    return <div key={task.id}>{task.title}</div>
+                })}
             </div>
+            <button onClick={addTask}>Adicionar Tarefa</button>
         </div>
     )
 }
 
 TaskList.propTypes = {
-    title : PropTypes.string.isRequired
+    title : PropTypes.string.isRequired,
+    onAddTask : propTypes.func.isRequired,
+    tasks: PropTypes.array.isRequired
 }
