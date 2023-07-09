76099b24f9c5a66826a6751f38b4cc4c81b38e6e
Michael Santos <mcialbr123@gmail.com>
2023-07-08 22:28:56 -0300

    del

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,20 +3,10 @@ import "./tasklist.css"
 import PropTypes from "prop-types";
 
 export default function TaskList({ title }) {    
-    const [count, setCount] = useState(0);
-
-    const increment = () => {
-         setCount((currentCount) =>{
-            return currentCount + 1
-         })
-    }
-
     return(
         <div className="tasklist">
             <div className="title">{title}</div>
             <div className="content">
-                {count}
-                <button onClick={increment}>incrementar</button>
             </div>
         </div>
     )
