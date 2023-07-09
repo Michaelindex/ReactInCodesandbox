4b7711a7ac6119a9bd0e034ba4326592f66cc6f2
Michael Santos <113257529+Michaelindex@users.noreply.github.com>
2023-07-09 00:35:08 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Ola Mundo</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ul className="lista">
+        <li>Aprendendo React</li>
+        <li>Componentes</li>
+      </ul>
     </div>
   );
 }
